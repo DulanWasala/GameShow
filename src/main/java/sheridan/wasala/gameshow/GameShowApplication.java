002eabca7604137dd93fa34987f73fcfaa6dd6df
Dulan <DulanWasala@gmail.com>
2023-02-05 15:23:46 -0500

    Start

---
+++ src/main/java/sheridan/wasala/gameshow/GameShowApplication.java
@@ -0,0 +1,13 @@
+package sheridan.wasala.gameshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameShowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/wasala/gameshow/GameShowApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.wasala.gameshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
